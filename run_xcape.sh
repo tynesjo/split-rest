cb5ea845aba6cd3d3a07322dd67e799a836f9ccb
Johannes Tynes <tynesjo@gmail.com>
2019-06-12 13:56:26 +0000

    Adding simple xcape run script required by the layout

---
+++ run_xcape.sh
@@ -0,0 +1,2 @@
+#! /usr/bin/env bash
+xcape -e "Hyper_L=space;ISO_Level5_Shift=Tab;ISO_Level3_Shift=Return;Control_L=Escape;Control_R=Escape;Super_R=slash;Super_L=BackSpace;Hyper_R=apostrophe;Meta_L=period"
